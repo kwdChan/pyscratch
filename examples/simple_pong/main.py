a380cd4f3a984ab9e38a0987d0739d8e05e1451c
kawa <kwd525@pm.me>
2025-05-29 22:19:42 +1000

    some comments

--- examples/simple_pong/main.py
+++ examples/simple_pong/main.py
@@ -23,15 +23,15 @@ font = pygame.font.SysFont(None, 48)  # None = default font, 48 = font size
 # 1. create left paddle
 left_paddle_sprite = pysc.rect_sprite(paddle_colour, paddle_width, paddle_height, pos=(paddle_margin, SCREEN_HEIGHT//2))
 pysc.game.add_sprite(left_paddle_sprite)
-left_paddle_sprite.set_collision_type(1)
+left_paddle_sprite.set_collision_type(1) # enables the collision
 
 
 ## behaviour
 ## - move by key 'a' and 'd' in a limited space
 
-left_timer_event = pysc.game.create_timer_trigger(10)
+left_timer_event = pysc.game.create_timer_trigger(10) # run every 10ms, repeats=np.inf by default
 
-def check_move_left(n):
+def check_move_left(n): # the parameter n is the number of repeats left in the trigger. unused in this case.
     movement = 0
     if pysc.sensing.is_key_pressed('w'):
         movement -= 8
@@ -80,9 +80,10 @@ right_timer_event.on_reset(check_move_right)
 
 
 # 3. Create the function that the ball
-
+# the edges are just sprites
 top_edge, left_edge, bottom_edge, right_edge = pysc.game.create_edges()
 
+# variables shared across the entire game
 pysc.game.shared_data['score_left'] = 0
 pysc.game.shared_data['score_right'] = 0
 
@@ -106,25 +107,27 @@ def spawn_ball():
     # random initial velocity
     vx = pysc.helper.random_number(0.7, 1.2) * ((pysc.helper.random_number(0, 1)>0.5)-0.5)
     vy = pysc.helper.random_number(-1, 1)
-    ball_sprite.body.velocity = (vx, vy)
+    ball_sprite.body.velocity = (vx, vy) # a setter method should be added for simplicity
 
     # behaviour: 
     ## 1. ball hit edge L: score
     ## 2. ball hit edge R: score
     ## 3. ball hit paddle L: sound effect
     ## 4. ball hit paddle R: sound effect
-
+    
+    # detect collisions between two specific sprites
     left_edge_event = pysc.game.create_specific_collision_trigger(ball_sprite, left_edge)
     right_edge_event = pysc.game.create_specific_collision_trigger(ball_sprite, right_edge)
 
     left_paddle_event = pysc.game.create_specific_collision_trigger(ball_sprite, left_paddle_sprite)
     right_paddle_event = pysc.game.create_specific_collision_trigger(ball_sprite, right_paddle_sprite)
 
+    # the "a" parameter is a pymunk object that contains the collision infomation. unused in this case
     def on_hit_left_edge(a):
 
         pysc.game.shared_data['score_right'] += 1 
         pysc.game.remove_sprite(ball_sprite)
-        pysc.game.boardcast_message('restart', None)
+        pysc.game.boardcast_message('restart', None) # boardcast message 
 
         # optional: resources release 
         left_edge_event.remove()
@@ -143,8 +146,7 @@ def spawn_ball():
         right_edge_event.remove()        
         left_paddle_event.remove()        
         right_paddle_event.remove()        
-
-
+    
     left_paddle_event.add_callback(lambda a: pysc.game.play_sound('bong', 0.5))
     right_paddle_event.add_callback(lambda a: pysc.game.play_sound('bong', 0.5))
 
@@ -154,14 +156,14 @@ def spawn_ball():
 
 
 # Score display
-score_left = rect_sprite((170, 170, 170), 100, 50, pos=(100, SCREEN_HEIGHT//2))
+score_left = rect_sprite((170, 170, 170), width=100, height=50, pos=(100, SCREEN_HEIGHT//2))
 pysc.game.add_sprite(score_left)
 
-score_right = rect_sprite((170, 170, 170), 100, 50, pos=(SCREEN_WIDTH-100, SCREEN_HEIGHT//2))
+score_right = rect_sprite((170, 170, 170), width=100, height=50, pos=(SCREEN_WIDTH-100, SCREEN_HEIGHT//2))
 pysc.game.add_sprite(score_right)
 
 # 
-def show_score(n):
+def show_score(data): # this function is called by the message trigger, which pass in some arbitory data. unused in this case 
     """
     show the score
 
@@ -170,15 +172,19 @@ def show_score(n):
     pysc.game.show_sprite(score_left)
     pysc.game.show_sprite(score_right)
 
+    # if do offset = (0, 0), the top-left corner of the text will 
+    # be at the top-left corner of the image of the sprite
     score_left.write_text(str(pysc.game.shared_data['score_left']), font, offset=(40, 10))
     score_right.write_text(str(pysc.game.shared_data['score_right']), font, offset=(40, 10))
 
-    
+    # a conditional trigger takes a function that output a boolean 
+    # the condition is checked every iteration of the game loop
     player_ready_event = pysc.game.create_conditional_trigger(
         lambda: pysc.sensing.is_key_pressed('space'),
-        repeats=1)
+        repeats=1
+    ) 
 
-    def on_player_ready(n):
+    def on_player_ready(n): # the n parameter is how many repeats left in the trigger
         spawn_ball()
         pysc.game.hide_sprite(score_left)
         pysc.game.hide_sprite(score_right)
@@ -188,7 +194,7 @@ def show_score(n):
 
 
 pysc.game.create_messager_trigger('restart').add_callback(show_score)
-pysc.game.boardcast_message('restart', None) # the message can passes data to the callback function
+pysc.game.boardcast_message('restart', None) # the message can pass data to the callback function
 
 pysc.game.update_screen_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pysc.game.start(60)
