fa155678f8e95d79bff13285fa65e1f1a8e3b110
kawa <kwd525@pm.me>
2025-06-10 22:32:14 +1000

    examples

---
+++ examples/getting-started/enemy_func_version.py
@@ -0,0 +1,30 @@
+import pyscratch as pysc
+from settings import *
+
+player = pysc.game.shared_data['player']
+
+#from typing import cast
+#from pyscratch.scratch_sprite import ScratchSprite
+#player = cast(ScratchSprite, player)
+
+def create_enemy(_):
+    enemy = pysc.create_single_costume_sprite("assets/fishes/18.png")
+    enemy.set_scale(.5)
+    enemy.x = pysc.helper.random_number(0, SCREEN_WIDTH)
+
+
+    if pysc.helper.random_number(0, 1) > 0.5: 
+        enemy.y = 0
+    else:
+        enemy.y = SCREEN_HEIGHT
+
+    def movement(_):
+        while True:
+            enemy.point_towards_sprite(player)
+            enemy.move_indir(1)
+            yield 1/FRAMERATE
+    # TODO: soft remove the event so the generator still get run 
+    enemy.when_timer_above(0).add_callback(movement)
+
+
+pysc.game.when_timer_reset(2).add_callback(create_enemy)
---
+++ examples/getting-started/player.py
@@ -0,0 +1,76 @@
+import pyscratch as pysc
+from settings import *
+
+
+player = pysc.create_single_costume_sprite("assets/fishes/107.png")
+player.set_draggable(True)
+player.set_rotation_style_left_right()
+
+# def movement():
+#     while True:
+#         if pysc.sensing.is_key_pressed('w'):
+#             player.y -= 4
+
+#         if pysc.sensing.is_key_pressed('s'):
+#             player.y += 4
+
+#         if pysc.sensing.is_key_pressed('a'):
+#             player.direction = 180
+#             player.x -= 4
+            
+#         if pysc.sensing.is_key_pressed('d'):
+#             player.direction = 0
+#             player.x += 4
+
+#         yield 1/60
+
+
+def movement():
+    speed_decay = 0.9
+    speed_y = 0
+    speed_x = 0
+
+    while True:
+        if pysc.sensing.is_key_pressed('w'):
+            speed_y = -4
+
+        elif pysc.sensing.is_key_pressed('s'):
+            speed_y = 4
+        else:
+            speed_y *= speed_decay
+
+        if pysc.sensing.is_key_pressed('a'):
+            player.direction = 180
+            speed_x = -4
+            
+        elif pysc.sensing.is_key_pressed('d'):
+            player.direction = 0
+            speed_x = 4
+        else:
+            speed_x *= speed_decay
+
+
+        player.y += speed_y
+        player.x += speed_x
+
+        yield 1/60
+
+
+game_start_event = player.when_game_start()
+game_start_event.add_callback(movement)
+
+
+
+def check_health():
+
+    while True:
+        if not pysc.game.shared_data['health']:
+            player.remove()
+        yield 1/60
+
+
+game_start_event.add_callback(check_health)
+
+
+pysc.game.shared_data['player'] = player
+
