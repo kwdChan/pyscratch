59270e337eb6e9950e0865fdc1b6acd65a789c96
kawa <kwd525@pm.me>
2025-06-14 17:25:00 +1000

    example changes

--- examples/simple_pong/left_paddle.py
+++ examples/simple_pong/left_paddle.py
@@ -2,7 +2,7 @@ import pyscratch as pysc
 from settings import *
 
 # TODO: bad naming consistency 
-sprite = pysc.create_rect_sprite(paddle_colour, paddle_width, paddle_height,  pos=(paddle_margin, SCREEN_HEIGHT//2))
+sprite = pysc.create_rect_sprite(paddle_colour, paddle_width, paddle_height,  position=(paddle_margin, SCREEN_HEIGHT//2))
 
 def movement():
     speed = 0
