b21b05a49fd5dba27211f04a6285b1fb3e72618b
kawa <kwd525@pm.me>
2025-06-20 22:12:41 +1000

    button to set sprite folder + file name line break

--- pyscratch/tools/sprite_preview/main.py
+++ pyscratch/tools/sprite_preview/main.py
@@ -3,7 +3,7 @@ import pyscratch as pysc
 from pyscratch.tools.sprite_preview import input_box
 from settings import *
 from right_panel import file_display_area, cut_or_back_button, cut_or_nav_switch, spritesheet_view, cut_parameters
-from main_panel import sprite_edit_ui, play_edit_ui, animation_display, frame_bin
+from main_panel import sprite_edit_ui, play_edit_ui, animation_display, frame_bin, set_as_sprite_folder
 from left_panel import frame_preview_card, frame_preview_panel
 
 bg = pysc.create_rect((221, 221, 221), SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -13,6 +13,6 @@ pysc.game.switch_backdrop(0)
 
 pysc.game.broadcast_message('folder_update', Path('testing_assets'))
 pysc.game.broadcast_message('cut_or_nav_mode_change', 'nav')
-pysc.game.broadcast_message('change_sprite_selection', Path('./testing_assets/cat'))
+#pysc.game.broadcast_message('change_sprite_selection', Path('./testing_assets/cat'))
 
 pysc.game.start(30)
