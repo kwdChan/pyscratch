37ccab719064686c8eae569c5fd4e9f66d2400e4
kawa <kwd525@pm.me>
2025-06-22 19:22:04 +1000

    removed the unnecessary files for the gh-pages

--- pyscratch/tools/sprite_preview/right_panel/cut_parameters.py
+++
@@ -1,9 +0,0 @@
-from pyscratch.tools.sprite_preview.input_box import IntegerInputBox
-import pyscratch as pysc
-from settings import *
-
-row_input = IntegerInputBox('n_row')
-row_input.set_xy((900, 650))
-
-col_input = IntegerInputBox('n_col')
-col_input.set_xy((1200, 650))
