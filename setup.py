36aefd6153697eb5c01c8850561b03953ba39f16
kawa <kwd525@pm.me>
2025-06-24 21:31:49 +1000

    adding typing_extensions as a dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyscratch',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'pymunk',
         'pygame',
-        'numpy'
+        'numpy',
+        'typing_extensions'
     ]
 )
