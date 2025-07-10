9ab2e797428803b451a0d71312078bbbe185066f
kawa <kwd525@pm.me>
2025-07-10 20:13:01 +1000

    remove the steps for pong

--- examples/simple_pong/step 4/additional_display.py
+++
@@ -1,4 +0,0 @@
-import pyscratch as pysc
-import pygame
-font = pygame.font.SysFont(None, 48)  # None = default font, 48 = font size
-pysc.create_shared_data_display_sprite('left_score', font, size=(300, 60))
