7eac73170ff97144a0c88cecfef4b2728fcde912
kawa <kwd525@pm.me>
2025-07-10 20:13:01 +1000

    remove the steps for pong

--- examples/simple_pong/step 3/edges.py
+++
@@ -1,9 +0,0 @@
-import pyscratch as pysc
-game = pysc.game 
-
-top_edge, left_edge, bottom_edge, right_edge = pysc.create_edge_sprites()
-
-game['top_edge'] = top_edge
-game['left_edge'] = left_edge
-game['bottom_edge'] = bottom_edge
-game['right_edge'] = right_edge
